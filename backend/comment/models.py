3a55efad7f4e8f65c3ecfadc8c995e7f9bb0028b
unknown <dylanjlenz@gmail.com>
2022-10-13 12:44:16 -0500

    Created Comments Model In Models.py

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.forms import CharField, IntegerField
+from authentication.models import User
 
 # Create your models here.
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = CharField(max_length=30)
+    text = CharField(max_length=30)
+    likes = IntegerField()
+    dislikes = IntegerField()
