3943be1b17e1d89f15683933f3d370b5b580dcde
unknown <dylanjlenz@gmail.com>
2022-10-17 10:51:35 -0500

    Added Path for user_comments in urls

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from backend.comments.views import user_comments
 from comments import views
 
 urlpatterns = [
-    path('all/', views.get_all_comments)
+    path('all/', views.get_all_comments),
+    path('', views.user_comments)
 ]
